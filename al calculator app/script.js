d6da36f3b5944d222a95c6d3bc8bed0dfb40fbb6
AlishbaGhori <126356642+AlishbaGhori@users.noreply.github.com>
2023-08-15 05:53:15 -0700

    Add files via upload

---
+++ al calculator app/script.js
@@ -0,0 +1,37 @@
+/** Implement a click function to which writes digits on calculator screen */
+
+/** Implement a function which clears digits from calculator screen upon clicking AC button */
+
+/** Implement a function which enters digit to screen upon pressing any number button and decimal point */
+
+/** Implement a function for adding/subtracting/multiplying/dividing first entered number */
+
+/** */
+// let string = '';
+// alert('Hello');
+// const buttons = querySelectorAll('.button');
+// Array.from(buttons).forEach((button) => {
+//   button.addEventListener('click', (e) => {});
+// });
+let string = '';
+const buttons = document.querySelectorAll('button');
+const clearScreen = () => {
+    string = '';
+    document.getElementById('screentext').innerHTML = string;
+  }
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const buttonText = e.target.innerHTML;
+    if(buttonText === '='){
+        document.getElementById('screentext').innerHTML = eval(string);
+    }else if(buttonText === 'AC'){
+        clearScreen()
+    }
+   
+    
+    else{
+        string = string+buttonText;
+        document.getElementById('screentext').innerHTML = string;
+    }
+  });
+});
